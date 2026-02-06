60f7a0c414ea87c0abffc92fbd1ac03227d13d0a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-02-06 16:31:00 +0300

    Add `BVector4`

---
+++ src/vectors/boolean/bvector4.ts
@@ -0,0 +1,17 @@
+export class BVector4 {
+
+  x: boolean
+
+  y: boolean
+
+  z: boolean
+
+  w: boolean
+
+  constructor(x: boolean = false, y: boolean = false, z: boolean = false, w: boolean = false) {
+    this.x = x
+    this.y = y
+    this.z = z
+    this.w = w
+  }
+}
