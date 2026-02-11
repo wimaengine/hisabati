68372658c740f95c675673df111985d98f90d5ae
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-02-11 18:24:00 +0300

    Add `mapToIndex3D`

--- src/functions/indexmap.ts
+++ src/functions/indexmap.ts
@@ -4,3 +4,14 @@ export function mapToIndex2D(value: number, width: number): Vector2 {
   return new Vector2(value % width, Math.floor(value / width))
 }
 
+
+
+export function mapToIndex3D(index: number, width: number, height: number): Vector3 {
+  const depthMax = width * height
+  const rem = index % depthMax
+  const z = Math.floor(index / depthMax)
+  const y = Math.floor(rem / width)
+  const x = rem % width
+
+  return new Vector3(x, y, z)
+}
