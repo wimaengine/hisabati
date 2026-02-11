2c79fa302bd2da3a28bc63d8c53b95239581c556
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-02-11 14:21:00 +0300

    Add `smootherStep`

--- src/functions/easing.ts
+++ src/functions/easing.ts
@@ -2,3 +2,8 @@ export function smoothStep(value: number): number {
   return value * value * (3 - 2 * value)
 }
 
+
+
+export function smootherStep(value: number): number {
+  return value * value * value * (value * (value * 6 - 15) + 10)
+}
