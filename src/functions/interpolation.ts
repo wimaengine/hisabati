0d700722300da50bffed90880408c2ef668ee503
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-02-12 13:56:00 +0300

    Add `inverseLerp`

--- src/functions/interpolation.ts
+++ src/functions/interpolation.ts
@@ -2,3 +2,9 @@ export function lerp(from: number, to: number, t: number): number {
   return from + t * (to - from)
 }
 
+
+
+export function inverseLerp(from: number, to: number, value: number): number {
+  return (value - from) / (to - from)
+}
+
