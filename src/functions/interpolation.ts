496c5bef22578295ff97ad9256e7dcb7e998f1ec
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-02-12 15:36:00 +0300

    Add `remap`

--- src/functions/interpolation.ts
+++ src/functions/interpolation.ts
@@ -8,3 +8,9 @@ export function inverseLerp(from: number, to: number, value: number): number {
   return (value - from) / (to - from)
 }
 
+
+
+export function remap(v: number, x1: number, y1: number, x2: number, y2: number): number {
+  return x2 + v * (y2 - x2) / (y1 - x1)
+}
+
